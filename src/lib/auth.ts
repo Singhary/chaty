7ae392df2d5705dc27be869520d5d72ac9fdb58c
Singhary <singhary449@gmail.com>
2024-10-12 20:49:00 +0530

    Added sidebar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -51,7 +51,7 @@ export const authOptions:NextAuthOptions = {
                 session.user.id = token.id;
                 session.user.name = token.name;
                 session.user.email = token.email;
-                session.user.image = token.picture;
+                session.user.image = token.image as string;
             }
             return session;
         },
